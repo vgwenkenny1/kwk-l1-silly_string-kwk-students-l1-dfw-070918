c17e6334527bed70e043e11d4389e1483c9454a3
vgwenkenny1 <vgwenkenny1@gmail.com>
2018-07-09 21:15:30 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,7 +1,7 @@
 
 # Challenge 1: Fill this string in with your favorite word
 
-your_favorite_word = ""
+your_favorite_word = "cool"
 
 puts "My favorite word is #{your_favorite_word}"
 
@@ -9,7 +9,7 @@ puts "My favorite word is #{your_favorite_word}"
 
 favorite_word_in_all_caps = your_favorite_word # Put a . after your_favorite word and use a String method.
 
-puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
+puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}".upcase
 
 # Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
 
@@ -17,19 +17,20 @@ phrase = "IM NOT SHOUTING"
 
 lowercased_phrase = phrase # Put a . after phrase and use a String method.
 
-puts lowercased_phrase
+puts lowercased_phrase.downcase
 
 # Challenge 4: Count how many letters are in that big_word
 
 big_word = "supercalifragilisticexpialidocious"
 
-letter_count = big_word # Put a . after big_word and use a String method.
+letter_count = big_word.length # Put a . after big_word and use a String method.
 
 puts "There are #{letter_count} in #{big_word}"
 
 # Challenge 5: Add "wow" and "mom" to big_word
 
-bigger_word = big_word # Use String concatenation to add "wow" and "mom" to big_word
+bigger_word = big_word + "wow" + "mom"
+# Use String concatenation to add "wow" and "mom" to big_word
 
 new_letter_count = bigger_word.size
 
@@ -37,7 +38,7 @@ puts "There are now #{new_letter_count} in #{bigger_word}"
 
 # Challenge 6: Capitalize this sentence properly
 
-proper_sentence = "i really like programming." # Put a . after the string to capitalize it properly.
+proper_sentence = "i really like programming.".capitalize # Put a . after the string to capitalize it properly.
 
 puts proper_sentence
 
@@ -46,6 +47,6 @@ sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples 
 
 # Swap all `A` for `O`, as well as all `a` for `o`. You need to chain `gsub`s.
 
-sentence = sentence # Add your gsub chain here
+sentence = sentence.gsub("A","O").gsub("a","o") # Add your gsub chain here
 
 puts sentence
